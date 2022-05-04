8497268d87fbf3f3e0901e775487ef2de9765376
Gabriel Bertocci Pedrosa <92754336+bert0ccidev@users.noreply.github.com>
2022-05-04 03:36:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const linksSocialMedia = {
+  github: 'bert0ccidev',
+  twitter: 'bert0cci_dev',
+  instagram: 'bert0cci_dev'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userLogin.textContent = data.login
+      userImg.src = data.avatar_url
+    })
+}
+
+getGitHubProfileInfos()
